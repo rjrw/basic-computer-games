bc002cbbb75cda270a3bffb71472942c52c8b5f8
Robin Williams <robin@rjrw.net>
2022-12-05 21:33:59 +0000

    Start on parsing test

--- 00_Utilities/lua/basic.lua
+++ 00_Utilities/lua/basic.lua
@@ -16,13 +16,22 @@ local any = m.P(1);
 local space = m.S" \t\n"^0;
 local digit = m.R("09");
 local lineno = digit^1;
-local statement = m.P{
-   lineno * space * any^0,
+local gotostatement = m.P {
+   m.P("GOTO") * space * lineno
+};
+local statement = m.P {
+   gotostatement
+};
+local compoundstatement = m.P{
+   (statement * m.P(":"))^0 * statement
+};
+local numbered = m.P{
+   lineno * space * compoundstatement,
 };
 
 local count = 1;
 for line in file:lines() do
-   local mend = m.match(statement, line);
+   local mend = m.match(numbered, line);
    if not mend then
       io.write("Syntax Error\n");
       io.write(line, "\n");
