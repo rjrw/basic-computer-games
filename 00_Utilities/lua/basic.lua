b2be79d6e7f54cde6c4f72eedc25371ed4a9dc4c
Robin Williams <robin@rjrw.net>
2022-12-08 16:22:47 +0000

    Add more captures

--- 00_Utilities/lua/basic.lua
+++ 00_Utilities/lua/basic.lua
@@ -23,31 +23,31 @@ local file = assert(io.open(arg[narg]));
 local any = m.P(1);
 local space = m.S" \t\n"^0;
 local digit = m.R("09");
-local string_ = m.P("\"") * (any-m.P("\""))^0 * m.P("\"");
-local integer = digit^1;
+local string_ = m.P("\"") * m.C((any-m.P("\""))^0) * m.P("\"");
+local integer = m.C(digit^1);
 local varname = m.R("AZ")^1 * m.R("09")^0;
-local floatvar = varname;
-local stringvar = varname * m.P("$");
+local floatvar = m.C(varname);
+local stringvar = m.C(varname * m.P("$"));
 local anyvar = m.P { floatvar + stringvar };
-local lineno = digit^1;
+local lineno = m.C(digit^1);
 local gotostatement = m.P {
-   m.P("GO") * space * m.P("TO") * space * lineno * space
+   m.Cc("GOTO") * m.P("GO") * space * m.P("TO") * space * lineno * space
 };
 local gosubstatement = m.P {
-   m.P("GO") * space * m.P("SUB") * space * lineno * space
+   m.Cc("GOSUB") * m.P("GO") * space * m.P("SUB") * space * lineno * space
 };
 local nextlist = m.P {
    ( floatvar * space * m.P"," * space)^0 * floatvar * space
 };
 local nextstatement = m.P {
-   m.P("NEXT") * space * nextlist * space
-      + m.P("NEXT")
+   m.C(m.P("NEXT")) * space * nextlist * space
+      + m.C(m.P("NEXT"))
 };
 local endstatement = m.P {
-   m.P("END") * space
+   m.C(m.P("END")) * space
 };
 local returnstatement = m.P {
-   m.P("RETURN") * space
+   m.C(m.P("RETURN")) * space
 };
 local stringexpr = m.V"stringexpr";
 local stringassignment = m.V"stringassignment";
@@ -59,10 +59,10 @@ local inputlist = m.V"inputlist";
 local inputitem = m.V"inputitem";
 
 local comparisonop = m.P {
-   m.P("=") + m.P("<>") + m.P("<=") + m.P(">=") + m.P("<") + m.P(">")
+   m.C(m.P("=") + m.P("<>") + m.P("<=") + m.P(">=") + m.P("<") + m.P(">"))
 };
 local stringcomparisonop = m.P {
-   m.P("=") + m.P("<>")-- + m.P("<=") + m.P(">=") + m.P("<") + m.P(">")
+   m.C(m.P("=") + m.P("<>"))
 };
 local Sum = m.V"Sum";
 local Product = m.V"Product"
@@ -96,53 +96,53 @@ local basicline = m.P {
    gotostatement + gosubstatement + forstatement + nextstatement
       + ifstatement + endstatement + printstatement + numericassignment
       + returnstatement + stringassignment + dimstatement + inputstatement,
-   printstatement = m.Ct(m.C(m.P("PRINT")) * space * m.C(printlist)),
+   printstatement = m.Ct(m.C(m.P("PRINT")) * space * m.Ct(printlist)),
    stringlval = stringelement + stringvar,
    stringelement = stringvar * space * m.P("(") * space * exprlist * space * m.P(")"),
    stringassignment =
-      m.P("LET")^-1 * space *
+      m.Cc("LET") * m.P("LET")^-1 * space *
       stringlval * space * m.P("=") * space * stringexpr * space,
    stringexpr = string_ + stringvar,
    printexpr = stringexpr + expr,
-   printlist = (printexpr * space * (m.P(";")*space)^-1 )^0,
+   printlist = (printexpr * space * m.C((m.S(";,")*space))^-1 )^0,
    inputitem = stringlval + floatlval,
    inputlist = (inputitem * space * m.P(",")*space)^-1 * inputitem,
-   inputstatement = m.P("INPUT") * space *
+   inputstatement = m.C(m.P("INPUT")) * space *
       (stringexpr * space * m.P(";") * space)^-1
       * inputlist,
-   ifstatement = m.P("IF") * space * logicalexpr * space *
+   ifstatement = m.C(m.P("IF")) * space * logicalexpr * space *
       m.P("THEN") * space * ( lineno * space + statementlist ),
-   exprlist = ( expr * space * m.P(",") * space)^0 * expr,
+   exprlist = m.Ct(( expr * space * m.P(",") * space)^0 * expr),
    dimdef = anyvar * space * m.P("(") * space * exprlist * space * m.P(")"),
    dimlist = ( dimdef * space * m.P(",") * space)^0 * dimdef,
-   dimstatement = m.P("DIM") * space * dimlist,
+   dimstatement = m.C(m.P("DIM")) * space * dimlist,
    logicalexpr = Or,
-   Or = (And * space * m.P("OR") * space)^0 * And,
-   And = (Not * space * m.P("AND") * space)^0 * Not,
-   Not = (m.P("NOT") * space)^-1 *
+   Or = m.Cc("OR") * m.Ct((And * space * m.P("OR") * space)^0 * And),
+   And = m.Cc("AND") * m.Ct((Not * space * m.P("AND") * space)^0 * Not),
+   Not = (m.C(m.P("NOT")) * space)^-1 *
       ( comparison
 	   + m.P("(") * space * Or * space * m.P(")") ),
    comparison = expr * space * comparisonop * space * expr
       + stringexpr * space * stringcomparisonop * space * stringexpr,
    forstatement =
-      m.P("FOR") * space * floatvar * space * m.P("=") * space * expr
+      m.C(m.P("FOR")) * space * floatvar * space * m.P("=") * space * expr
       * space * m.P("TO") * space * expr * space *
       ( m.P("STEP") * space * expr * space )^-1,
    numericassignment =
-      m.P("LET")^-1 * space *
+      m.Cc("LETN") * m.P("LET")^-1 * space *
       floatlval * space * m.P("=") * space * expr * space,
    expr = Sum,
-   Sum = ( Product * space * m.S("+-") * space)^0 * Product * space,
-   Product = ( Unary * space * m.S("*/") * space)^0 * Unary * space,
-   Unary = m.S("+-")^-1 * Value,
+   Sum = ( Product * space * m.C(m.S("+-")) * space)^0 * Product * space,
+   Product = ( Unary * space * m.C(m.S("*/")) * space)^0 * Unary * space,
+   Unary = m.C(m.S("+-")^-1) * Value,
    Value = integer + floatlval + m.P("(") * space * Sum * m.P(")"),
    floatlval = element + floatvar,
    -- Array access/function/builtin call
    arg = expr + logicalexpr + stringexpr,
-   arglist = ( arg * space * m.P(",") * space)^0 * arg,
+   arglist = m.Ct(( arg * space * m.P(",") * space)^0 * arg),
    element = floatvar * space * m.P("(") * space * exprlist * space * m.P(")"),
    statementlist = m.Ct((statement * m.P(":") * space )^0 * statement),
-   line = m.Ct(m.C(lineno) * space * statementlist * m.Cp()),
+   line = m.Ct(lineno * space * statementlist * m.Cp()),
 };
 
 local prog = {};
@@ -175,7 +175,10 @@ if nerr == 0 and mode == 2 then
       local stats = m[2];
       for i=1,#stats do
 	 if stats[i][1] == "PRINT" then
-	    print(stats[i][2]);
+	    local printlist = stats[i][2];
+	    for j=1,#printlist do
+	       print(printlist[j]);
+	    end
 	 end
       end
    end
