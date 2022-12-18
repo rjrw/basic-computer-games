05c0546fecb6be218df5e416d738d28a97367981
Robin Williams <robin@rjrw.net>
2022-12-18 22:36:14 +0000

    Modifications to 78_Sine_Wave in Lua
     - "math" and "string" modules are loaded by default
     - Fix bug which meant only one word was printed: modify the
    coding to demonstrate the construction of an iterator which
    outputs successively from the elements of the input table.

--- 78_Sine_Wave/lua/sinewave.lua
+++ 78_Sine_Wave/lua/sinewave.lua
@@ -1,6 +1,3 @@
-require("math")
-require("string")
-
 print("\n                         Sine Wave")
 print("           Creative Computing Morriston, New Jersy")
 print("\n\n\n\n")
@@ -8,14 +5,29 @@ print("\n\n\n\n")
 -- Original BASIC version by David Ahl
 -- Ported to lua by BeeverFeever(github), 2022
 
-local toggleWord = true
+-- Factory which takes list of words, and generates a function which
+-- emits them in turn.
+function rotator(words)
+   assert(#words>0);
+   local w, pos = {}, 0;
+   -- Capture a copy of the input table, so that the list can't be
+   -- externally modified
+   for _,v in ipairs(words) do
+      w[#w+1] = v;
+   end
+   return function ()
+      pos = pos+1;
+      if pos > #w then
+	 pos = 1;
+      end
+      return w[pos];
+   end
+end
+
+local message = rotator{"Creative", "Computing"};
 
 for t = 0, 40, 0.25 do
     local gap = math.floor(26 + 25 * math.sin(t))
-    if toggleWord == true then
-        -- string.rep used to add the gat at the front of the printed out words
-        print(string.rep(" ", math.floor(gap)) .. "Creative")
-    elseif toggleWord == false then
-        print(string.rep(" ", math.floor(gap)) .. "Computing")
-    end
+    -- string.rep used to add the gap at the front of the printed out words
+    print(string.rep(" ", math.floor(gap)) .. message());
 end
