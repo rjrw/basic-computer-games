d0fc9bcd892034b738f7bcc6b4f55a9782c6f58b
Robin Williams <robin@rjrw.net>
2022-12-19 12:54:24 +0000

    Extract more functionality

--- 03_Animal/lua/animal.lua
+++ 03_Animal/lua/animal.lua
@@ -9,13 +9,14 @@ function banner()
 end
 
 local a = {
-   ":q Does it swim:y2:n3:", ":aFish",":aBird"
+   ":qDoes it swim:y2:n3:", ":aFish",":aBird"
 };
-local function list()
+
+local function list(a)
    io.write("\n\nThe animals I already know are\n");
    local x=0;
-   for i=1,#a do
-      local animal = a[i]:match("^:a(.*)");
+   for _,v in ipairs(a) do
+      local animal = v:match("^:a(.*)");
       if animal then
 	 io.write(animal);
 	 x = x+1;
@@ -30,12 +31,12 @@ local function list()
    io.write(x == 0 and "\n" or "\n\n");
 end
 
-local function prompt(s)
+local function prompt(a)
    repeat
       io.write("Are you thinking of an animal? ");
       local s = io.read():lower();
       if s == "list" then
-	 list();
+	 list(a);
       end
    until s:sub(1,1) == "y";
 end
@@ -44,46 +45,59 @@ local function nextquestion(a,k)
    local q = a[k];
    local c = "";
    repeat
-      local f = assert(q:find(":",3));
-      io.write (q:sub(3,f-1),"? ");
+      local f = assert(q:match("([^:]+)",3));
+      io.write (f,"? ");
       c = io.read():lower():sub(1,1);
    until c == "y" or c == "n";
-   local x = assert(q:match(":"..c.."([^:]+)",3));
+   local x = assert(q:match(":"..c.."(%d+)",3));
    return tonumber(x);
 end
 
--- Main control section
-while true do
-   prompt("Are you thinking of an animal");
-   
+local function selectanimal(a)
    local k = 1;
-
    repeat
       k = nextquestion(a,k);
       if a[k] == nil then os.exit(); end;
    until a[k]:sub(1,2) ~= ":q";
 
+   return k;
+end
+
+local function distinguishanimal(found)
+   io.write("The animal you were thinking of was a ? ");
+   local animal = io.read();
+   io.write("Please type in a question that would distinguish a ",
+	    animal," from a ",found,"? ");
+   local query = io.read();
+   local answer = "";
+   repeat
+      io.write("For a ",animal," the answer would be? ");
+      answer = io.read():sub(1,1):lower();
+   until answer == "y" or answer == "n";
+   return animal, query, answer;
+end
+
+local function addanimal(a,k,animal,query,answer)
+   local other = answer == "y" and "n" or "y";
+   local z1 = #a+1;
+   a[z1] = a[k];
+   a[z1+1] = ":a"..animal;
+   a[k] = ":q"..query..":"..
+      answer..tostring(z1+1)..":"..other..tostring(z1)..":";
+end
+
+-- Main control section
+while true do
+   prompt(a);
+
+   local k = selectanimal(a);
    local found = a[k]:sub(3);
    io.write("Is it a ",found,"? ");
    local s = io.read():sub(1,1):lower();
    if s == "y" then
       print("Why not try another animal?");
    else
-      io.write("The animal you were thinking of was a ? ");
-      local animal = io.read();
-      io.write("Please type in a question that would distinguish a ",
-	       animal," from a ",found," ? ");
-      local query = io.read();
-      local answer = "";
-      repeat
-	 io.write("For a ",animal," the answer would be? ");
-	 answer = io.read():sub(1,1):lower();
-      until answer == "y" or answer == "n";
-      local other = answer == "y" and "n" or "y";
-      local z1 = #a+1;
-      a[z1] = a[k];
-      a[z1+1] = ":a"..animal;
-      a[k] = ":q"..query..":"..
-	 answer..tostring(z1+1)..":"..other..tostring(z1)..":";
+      local animal, query, answer = distinguishanimal(found);
+      addanimal(a,k,animal,query,answer);
    end
 end
